58718028aad82317221859bd87b5e5a74b089270
vukasinstudge <vukasin.draskovic@gmai.com>
2020-05-21 05:23:36 +0200

    Malko dodao

---
+++ Projekat_Faza3_ver1/js/myJS.js
@@ -0,0 +1,7 @@
+var helper;
+
+function search() {
+    document.getElementById("found").style.display = "block";
+    document.getElementById("searchButton").style.display = "none";
+    document.getElementById("searchParagraph").textContent = "We found these devices";
+}
